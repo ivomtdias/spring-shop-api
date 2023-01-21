1641b9c0e0d997461dc825526442b3e7e5748e68
Ivo Dias <ivo.dias-ext@man.eu>
2023-01-21 19:31:19 +0000

    Initial commit

---
+++ src/main/java/com/ivomtdias/springshopapi/SpringShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.ivomtdias.springshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivomtdias/springshopapi/SpringShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivomtdias.springshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
