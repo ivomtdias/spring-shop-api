39bd591334fc84997d49beccc55f57e2fb5c95c1
Ivo Dias <ivo.dias-ext@man.eu>
2023-01-30 20:56:28 +0000

    Finish stock controller

--- src/main/java/com/ivomtdias/springshopapi/controller/StockController.java
+++ src/main/java/com/ivomtdias/springshopapi/controller/StockController.java
@@ -1,14 +1,13 @@
 package com.ivomtdias.springshopapi.controller;
 
+import com.ivomtdias.springshopapi.model.Stock;
 import com.ivomtdias.springshopapi.service.StockService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
@@ -33,5 +32,11 @@ public class StockController {
     public ResponseEntity<Void> removeStock(@PathVariable UUID productId, @PathVariable int quantityToRemove){
         stockService.removeStock(productId, quantityToRemove);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Secured({"ADMIN", "CUSTOMER"})
+    @GetMapping()
+    public ResponseEntity<Set<Stock>> getAllStock(){
+        return ResponseEntity.ok(stockService.findAll());
     }
 }
