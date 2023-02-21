ee5b0239d464872bd9d647b65402fe2758598880
Ivo Dias <ivo.dias-ext@man.eu>
2023-02-21 18:44:01 +0000

    Reverted signin method to return SignInResponse

--- src/main/java/com/ivomtdias/springshopapi/controller/AuthController.java
+++ src/main/java/com/ivomtdias/springshopapi/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.ivomtdias.springshopapi.controller;
 
 import com.ivomtdias.springshopapi.model.request.SignInRequest;
 import com.ivomtdias.springshopapi.model.request.SignUpRequest;
+import com.ivomtdias.springshopapi.model.response.SignInResponse;
 import com.ivomtdias.springshopapi.model.response.SignUpResponse;
 import com.ivomtdias.springshopapi.service.impl.AuthServiceImpl;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,7 @@ public class AuthController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<String> authenticate(@RequestBody SignInRequest request){
-        return ResponseEntity.ok(authService.signIn(request).getToken());
+    public ResponseEntity<SignInResponse> authenticate(@RequestBody SignInRequest request){
+        return ResponseEntity.ok(authService.signIn(request));
     }
 }
